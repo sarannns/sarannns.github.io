a4fb157c6c6b60e0ff5491e5d2b4bc531b35df6f
sarannns <87368592+sarannns@users.noreply.github.com>
2024-09-10 16:53:11 +0200

    particle render issue

--- js/script.js
+++ js/script.js
@@ -24,11 +24,11 @@ particlesJS("particles-js", {
       "type": "circle", "stroke": { "width": 0, "color": "#000000" },
       "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 }
     },
-    "opacity": { "value": 1, "random": false, "anim": { "enable": false, "speed": 0.1, "opacity_min": 0.1, "sync": false } },
-    "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.2, "sync": false } },
+    "opacity": { "value": 1, "random": false, "anim": { "enable": false, "speed": 0.2, "opacity_min": 0.1, "sync": false } },
+    "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 50, "size_min": 0.2, "sync": false } },
     "line_linked": { "enable": true, "distance": 150, "color": "#726a6a", "opacity": 0.4, "width": 1 },
     "move": {
-      "enable": true, "speed": 1, "direction": "none", "random": false, "straight": false, "out_mode": "out",
+      "enable": true, "speed": 10, "direction": "none", "random": false, "straight": false, "out_mode": "out",
       "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 }
     }
   },
