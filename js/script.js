041bb0bbc25df06d7ffd11ebb6a1b12796acc5fe
sarannns <87368592+sarannns@users.noreply.github.com>
2024-09-10 15:04:43 +0200

    particle speed change

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ particlesJS("particles-js", {
       "type": "circle", "stroke": { "width": 0, "color": "#000000" },
       "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 }
     },
-    "opacity": { "value": 1, "random": false, "anim": { "enable": false, "speed": 0.2, "opacity_min": 0.1, "sync": false } },
+    "opacity": { "value": 1, "random": false, "anim": { "enable": false, "speed": 0.1, "opacity_min": 0.1, "sync": false } },
     "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.2, "sync": false } },
     "line_linked": { "enable": true, "distance": 150, "color": "#726a6a", "opacity": 0.4, "width": 1 },
     "move": {
