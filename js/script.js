729885df85ecce21a9965a6375e0d39c1973b69e
sarannns <87368592+sarannns@users.noreply.github.com>
2024-09-11 21:06:46 +0200

    emulate multi device done

--- js/script.js
+++ js/script.js
@@ -46,7 +46,8 @@ particlesJS("particles-js", {
     }
   }, "retina_detect": false
 }); var count_particles,
-  stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute';
+  stats, update;
+stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute';
 stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles');
 update = function () {
