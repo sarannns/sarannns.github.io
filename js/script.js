1b708830992e0212750c4558a3eaa01f5d220e1f
sarannns <87368592+sarannns@users.noreply.github.com>
2024-09-15 14:19:38 +0200

    particle phys update

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ var typed = new Typed(".multiple-text",
 
 particlesJS("particles-js", {
   "particles": {
-    "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
+    "number": { "value": 40, "density": { "enable": true, "value_area": 800 } },
     "color": { "value": "#312a2a" }, "shape": {
       "type": "circle", "stroke": { "width": 0, "color": "#000000" },
       "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 }
